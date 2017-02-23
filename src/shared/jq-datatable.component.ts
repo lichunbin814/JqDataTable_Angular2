2c032f1ad29d40901aa6e3f45b3bc9357e0a7eb8
JohnLee <john.lee@jetfusion.com.tw>
2017-02-23 17:13:45 +0800

    讓Component能讀取指定的JS

--- src/shared/jq-datatable.component.ts
+++ src/shared/jq-datatable.component.ts
@@ -1,10 +1,14 @@
 import {
   Component,
   ElementRef,
-  AfterViewInit
+  AfterViewInit,
+  OnInit
 } from '@angular/core';
 
+
+
 declare var $: any;
+declare var System: any;
 
 @Component({
   selector: 'app-jq-datatable',
@@ -34,41 +38,50 @@ declare var $: any;
 </table>
 `,
 })
-export class JqueryDatatableComponent implements AfterViewInit {
+export class JqueryDatatableComponent implements OnInit, AfterViewInit {
 
   constructor(private el: ElementRef) {
 
   }
 
-  ngAfterViewInit() {
-    const element = $(this.el.nativeElement.children[0]);
+  ngOnInit() {
+    Promise.all([
+      System.import('script-loader!assets/jquery-datatables/scripts/jquery.dataTables.min.js'),
+    ]).then(() => {
+      // 初始化DataTable
+      const element = $(this.el.nativeElement.children[0]);
 
-    element.DataTable({
-      'processing': true,
-      'serverSide': true,
-      'ajax': {
-        'url': '/api/post.php',
-        'type': 'POST'
-      },
-      'columns': [
-        {
-          'data': '',
-          'render': function (data, type, row) {
-            return '<button class="row-btn">我是按鈕</button>';
-          },
+      element.DataTable({
+        'processing': true,
+        'serverSide': true,
+        'ajax': {
+          'url': '/api/post.php',
+          'type': 'POST'
         },
-        { 'data': 'first_name', },
-        { 'data': 'last_name' },
-        { 'data': 'position' },
-        { 'data': 'office' },
-        { 'data': 'start_date' },
-        { 'data': 'salary' }
-      ]
-    });
+        'columns': [
+          {
+            'data': '',
+            'render': function (data, type, row) {
+              return '<button class="row-btn">我是按鈕</button>';
+            },
+          },
+          { 'data': 'first_name', },
+          { 'data': 'last_name' },
+          { 'data': 'position' },
+          { 'data': 'office' },
+          { 'data': 'start_date' },
+          { 'data': 'salary' }
+        ]
+      });
 
-    element.on('click', '.row-btn', function () {
-      alert('已點擊(Angular2)');
+      element.on('click', '.row-btn', function () {
+        alert('已點擊(Angular2)');
+      });
     });
+  }
+
+  ngAfterViewInit() {
+
   };
 
 
